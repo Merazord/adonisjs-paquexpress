e74ffed01dccd2bc6a34add040c4ddb58fce1b95
Merazord <jose.angel.meraz@gmail.com>
2020-03-26 14:08:30 -0600

    Final, todos los errores corregidos

--- app/Controllers/Http/Paquexpress/UsuarioController.js
+++ app/Controllers/Http/Paquexpress/UsuarioController.js
@@ -3,87 +3,83 @@ const User = use('App/Models/User')
 const Token = use('App/Models/Token')
 class UsuarioController {
 
-  async registrar ({ request, response }) {
+  async registrar({ request, response }) {
     const objeto = request.all();
-     try {
-         const user = new User()
-         user.email = objeto.email;
-         user.username = objeto.username;
-         user.password = objeto.password;
-         await user.save()
-         return response.status(201).json({
-             message: 'usuario creado con exito',
-             user: user
-         })
-     } catch (error) {
-         return response.status(406).json({
-             message: 'There was a problem creating the user, p  lease try again later.'
-         })
-     }
- }
- async index ({request,auth, response })
- {
-   let user = await User.all()
-   return response.json(user);
- }
- async iniciar ({ request,auth, response }) {
+    try {
+      const user = new User()
+      user.email = objeto.email;
+      user.username = objeto.username;
+      user.password = objeto.password;
+      await user.save()
+      return response.status(201).json({
+        message: 'usuario creado con exito',
+        user: user
+      })
+    } catch (error) {
+      return response.status(406).json({
+        message: 'There was a problem creating the user, p  lease try again later.'
+      })
+    }
+  }
+  async index({ request, auth, response }) {
+    let user = await User.all()
+    return response.json(user);
+  }
+  async iniciar({ request, auth, response }) {
 
-  const { email, password } = request.only(['email', 'password']);
-  const token = await auth.attemp(email,password);
-   return response.json(token);
-}
+    const { email, password } = request.only(['email', 'password'])
 
-  async cerrar ({ request,auth, response }) {
-    try{
-     const apiToken = auth.getAuthHeader()
-     // Revoketokenforuser TODOS LOS TOKEN REVOKADOS
-     await auth
-       .authenticator('api')
-       .revokeTokens([apiToken])
-        return response.status(200).json({
-         message: 'Token revokado'
+    const token = await auth.attempt(email, password)
 
-     })
-    }catch(e)
-    {
-     return response.status(500).json({
-         message: 'Ocurrio un error',
-         error: event
+    return response.json(token)
+  }
 
-     })
+  async cerrar({ request, auth, response }) {
+    try {
+      const apiToken = auth.getAuthHeader()
+      // Revoketokenforuser TODOS LOS TOKEN REVOKADOS
+      await auth
+        .authenticator('api')
+        .revokeTokens([apiToken])
+      return response.status(200).json({
+        message: 'Token revokado'
+
+      })
+    } catch (e) {
+      return response.status(500).json({
+        message: 'Ocurrio un error',
+        error: event
+
+      })
     }
 
   }
-  async Insert ({request,response})
-  {
-     const objeto = request.all();
-         const user = new User()
-         user.email = objeto.email;
-         user.username = objeto.username;
-         user.password = objeto.password;
-         if(await user.save())
-         {
-           return response.status(201).json({
-            message: 'usuario creado con exito',
-            user: user
-        })
-
-         }
-         return response.status(500).json({
-          message: 'Ocurrio un error'
+  async Insert({ request, response }) {
+    const objeto = request.all();
+    const user = new User()
+    user.email = objeto.email;
+    user.username = objeto.username;
+    user.password = objeto.password;
+    if (await user.save()) {
+      return response.status(201).json({
+        message: 'usuario creado con exito',
+        user: user
       })
+
+    }
+    return response.status(500).json({
+      message: 'Ocurrio un error'
+    })
 
   }
 
-  async edit({params,response})
-  {
+  async edit({ params, response }) {
     const user = await User.find(params.id)
     return response.status(200).json({
       Usuario: user
-  })
+    })
   }
-  async update({params,request,response})
-  {
+  async update({ params, request, response }) {
     const user = await User.find(params.id)
     const objeto = request.all();
     user.username = objeto.username
@@ -92,15 +88,14 @@ class UsuarioController {
     await user.save()
     return response.status(200).json({
       Usuario: 'Usuario actualizado con exito!'
-  })
+    })
   }
-  async delete({params,request,response})
-  {
+  async delete({ params, request, response }) {
     const user = await User.find(params.id)
     await user.delete();
     return response.status(200).json({
       Usuario: 'Usuario borrado con exito!'
-  })
+    })
 
   }
 
--- start/routes.js
+++ start/routes.js
@@ -20,70 +20,70 @@ Route.get('/', () => {
 })
 
 Route.group(() => {
-Route.post('registrar', 'Paquexpress/UsuarioController.registrar');
-// Generar y Revocar token
-Route.post('iniciar', 'Paquexpress/UsuarioController.iniciar');
-Route.get('cerrar', 'Paquexpress/UsuarioController.cerrar') .middleware(['auth:api'])
+  Route.post('registrar', 'Paquexpress/UsuarioController.registrar');
+  // Generar y Revocar token
+  Route.post('iniciar', 'Paquexpress/UsuarioController.iniciar');
+  Route.get('cerrar', 'Paquexpress/UsuarioController.cerrar').middleware(['auth:api'])
 
-// Usuarios testeado
-Route.get('Usuarios','Paquexpress/UsuarioController.index');
-Route.post('Usuarios','Paquexpress/UsuarioController.Insert');
-Route.get('Usuarios/:id','Paquexpress/UsuarioController.edit');
-Route.put('Usuarios/:id','Paquexpress/UsuarioController.update');
-Route.delete('Usuarios/:id','Paquexpress/UsuarioController.delete');
+  // Usuarios testeado
+  Route.get('Usuarios', 'Paquexpress/UsuarioController.index');
+  Route.post('Usuarios', 'Paquexpress/UsuarioController.Insert');
+  Route.get('Usuarios/:id', 'Paquexpress/UsuarioController.edit');
+  Route.put('Usuarios/:id', 'Paquexpress/UsuarioController.update');
+  Route.delete('Usuarios/:id', 'Paquexpress/UsuarioController.delete');
 
-// // // Ciudades testeado
-Route.get('Ciudades','Paquexpress/CiudadeController.index');
-Route.post('Ciudades','Paquexpress/CiudadeController.Insert');
-Route.get('Ciudades/:id','Paquexpress/CiudadeController.edit');
-Route.put('Ciudades/:id','Paquexpress/CiudadeController.update');
-Route.delete('Ciudades/:id','Paquexpress/CiudadeController.delete');
+  // // // Ciudades testeado
+  Route.get('Ciudades', 'Paquexpress/CiudadeController.index');
+  Route.post('Ciudades', 'Paquexpress/CiudadeController.Insert');
+  Route.get('Ciudades/:id', 'Paquexpress/CiudadeController.edit');
+  Route.put('Ciudades/:id', 'Paquexpress/CiudadeController.update');
+  Route.delete('Ciudades/:id', 'Paquexpress/CiudadeController.delete');
 
-// // // Productos testeado
-Route.get('Productos','Paquexpress/ProductoController.index');
-Route.post('Productos','Paquexpress/ProductoController.Insert');
-Route.get('Productos/:id','Paquexpress/ProductoController.edit');
-Route.put('Productos/:id','Paquexpress/ProductoController.update');
-Route.delete('Productos/:id','Paquexpress/ProductoController.delete');
+  // // // Productos testeado
+  Route.get('Productos', 'Paquexpress/ProductoController.index');
+  Route.post('Productos', 'Paquexpress/ProductoController.Insert');
+  Route.get('Productos/:id', 'Paquexpress/ProductoController.edit');
+  Route.put('Productos/:id', 'Paquexpress/ProductoController.update');
+  Route.delete('Productos/:id', 'Paquexpress/ProductoController.delete');
 
-// // // Transportes testeado
-Route.get('Transportes','Paquexpress/TransporteController.index');
-Route.post('Transportes','Paquexpress/TransporteController.Insert');
-Route.get('Transportes/:id','Paquexpress/TransporteController.edit');
-Route.put('Transportes/:id','Paquexpress/TransporteController.update');
-Route.delete('Transportes/:id','Paquexpress/TransporteController.delete');
+  // // // Transportes testeado
+  Route.get('Transportes', 'Paquexpress/TransporteController.index');
+  Route.post('Transportes', 'Paquexpress/TransporteController.Insert');
+  Route.get('Transportes/:id', 'Paquexpress/TransporteController.edit');
+  Route.put('Transportes/:id', 'Paquexpress/TransporteController.update');
+  Route.delete('Transportes/:id', 'Paquexpress/TransporteController.delete');
 
-// // // Vendedores testeado
-Route.get('Vendedores','Paquexpress/VendedoreController.index');
-Route.post('Vendedores','Paquexpress/VendedoreController.Insert');
-Route.get('Vendedores/:id','Paquexpress/VendedoreController.edit');
-Route.put('Vendedores/:id','Paquexpress/VendedoreController.update');
-Route.delete('Vendedores/:id','Paquexpress/VendedoreController.delete');
+  // // // Vendedores testeado
+  Route.get('Vendedores', 'Paquexpress/VendedoreController.index');
+  Route.post('Vendedores', 'Paquexpress/VendedoreController.Insert');
+  Route.get('Vendedores/:id', 'Paquexpress/VendedoreController.edit');
+  Route.put('Vendedores/:id', 'Paquexpress/VendedoreController.update');
+  Route.delete('Vendedores/:id', 'Paquexpress/VendedoreController.delete');
 
-// // // Envios testeado
-Route.get('Envios','Paquexpress/EnvioController.index');
-Route.post('Envios','Paquexpress/EnvioController.Insert');
-Route.get('Envios/:id','Paquexpress/EnvioController.edit');
-Route.put('Envios/:id','Paquexpress/EnvioController.update');
-Route.delete('Envios/:id','Paquexpress/EnvioController.delete');
+  // // // Envios testeado
+  Route.get('Envios', 'Paquexpress/EnvioController.index');
+  Route.post('Envios', 'Paquexpress/EnvioController.Insert');
+  Route.get('Envios/:id', 'Paquexpress/EnvioController.edit');
+  Route.put('Envios/:id', 'Paquexpress/EnvioController.update');
+  Route.delete('Envios/:id', 'Paquexpress/EnvioController.delete');
 
-Route.get('Dashboard','Paquexpress/EnvioController.dashboard');
+  Route.get('Dashboard', 'Paquexpress/EnvioController.dashboard');
 
-// // // Envios testeado
-Route.get('Categorias','Paquexpress/CategoriaController.index');
-Route.post('Categorias','Paquexpress/CategoriaController.Insert');
-Route.get('Categorias/:id','Paquexpress/CategoriaController.edit');
-Route.put('Categorias/:id','Paquexpress/CategoriaController.update');
-Route.delete('Categorias/:id','Paquexpress/CategoriaController.delete');
+  // // // Envios testeado
+  Route.get('Categorias', 'Paquexpress/CategoriaController.index');
+  Route.post('Categorias', 'Paquexpress/CategoriaController.Insert');
+  Route.get('Categorias/:id', 'Paquexpress/CategoriaController.edit');
+  Route.put('Categorias/:id', 'Paquexpress/CategoriaController.update');
+  Route.delete('Categorias/:id', 'Paquexpress/CategoriaController.delete');
 
-// // // Envios testeado
-Route.get('TipoEnvios','Paquexpress/TipoEnvioController.index');
-Route.post('TipoEnvios','Paquexpress/TipoEnvioController.Insert');
-Route.get('TipoEnvios/:id','Paquexpress/TipoEnvioController.edit');
-Route.put('TipoEnvios/:id','Paquexpress/TipoEnvioController.update');
-Route.delete('TipoEnvios/:id','Paquexpress/TipoEnvioController.delete');
+  // // // Envios testeado
+  Route.get('TipoEnvios', 'Paquexpress/TipoEnvioController.index');
+  Route.post('TipoEnvios', 'Paquexpress/TipoEnvioController.Insert');
+  Route.get('TipoEnvios/:id', 'Paquexpress/TipoEnvioController.edit');
+  Route.put('TipoEnvios/:id', 'Paquexpress/TipoEnvioController.update');
+  Route.delete('TipoEnvios/:id', 'Paquexpress/TipoEnvioController.delete');
 
 
-// // Clima
-Route.post('Clima','Paquexpress/ClimaController.consultar');
- }).prefix('api')
+  // // Clima
+  Route.post('Clima', 'Paquexpress/ClimaController.consultar');
+}).prefix('api')
